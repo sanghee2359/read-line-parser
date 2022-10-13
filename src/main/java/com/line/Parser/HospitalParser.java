42a5dd913446a5bd09ff8880bc8822985a7d6822
sanghee <wjdtkdgml7352@daum.net>
2022-10-13 13:21:35 +0900

    [개선] str을 받을 때부터 따옴표 제거

--- src/main/java/com/line/Parser/HospitalParser.java
+++ src/main/java/com/line/Parser/HospitalParser.java
@@ -4,13 +4,12 @@ import com.line.domain.Hospital;
 
 // Parse 의 구현체
 public class HospitalParser implements Parser<Hospital>{ // type = Hospital
-    private String replaceAllQuot(String str){
-        return str.replaceAll("\"", "");
-    }
+
     @Override
     public Hospital parse(String str) {
+        str= str.replaceAll("\"", "");
         String[] splitted = str.split(",");
-        return new Hospital(replaceAllQuot(splitted[0]), replaceAllQuot(splitted[1]), replaceAllQuot(splitted[3]),
-                replaceAllQuot(splitted[6]), replaceAllQuot(splitted[10]), replaceAllQuot(splitted[7]));  // hospital 객체 생성
+        return new Hospital(splitted[0], splitted[1], splitted[3],
+                splitted[6], splitted[10], splitted[7]);  // hospital 객체 생성
     }
 }
